de46477b942b5e249a6f141b38ad65ecd28f139c
Aravindakumar V M <aravindakumar_vijayasrimohankumar@intuit.com>
2024-09-19 14:57:11 -0700

    item app  with react - redux - thunk

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const baseUrl = "http://localhost:3000";
-
-function enableDevTools() {
-  return (
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+function ID() {
+  return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-class PhotoAPI {
-  url = `${baseUrl}/photos`;
+class Item {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+const baseUrl = 'http://localhost:3000';
+
+class ItemAPI {
+  url = `${baseUrl}/items`;
 
   constructor() {}
 
@@ -17,19 +22,55 @@ class PhotoAPI {
       .then(this.parseJSON);
   }
 
+  add(item) {
+    return fetch(`${this.url}`, {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(this.checkStatus)
+      .then(this.parseJSON);
+  }
+
+  update(item) {
+    return fetch(`${this.url}/${item.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(this.checkStatus)
+      .then(this.parseJSON);
+  }
+
+  delete(id) {
+    return fetch(`${this.url}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(this.checkStatus)
+      .then(this.parseJSON);
+  }
+
   static translateStatusToErrorMessage(status) {
     switch (status) {
       case 401:
-        return "Please login again.";
+        return 'Please login again.';
       case 403:
-        return "You do not have permission to view the photos.";
+        return 'You do not have permission to view the items.';
       default:
-        return "There was an error retrieving the photos. Please try again.";
+        return 'There was an error retrieving the items. Please try again.';
     }
   }
 
+  //pass translate in to make this more flexible
   checkStatus(response) {
-    if (response.ok) {
+    if (response.status >= 200 && response.status < 300) {
       return response;
     } else {
       const httpErrorInfo = {
@@ -41,7 +82,7 @@ class PhotoAPI {
         `logging http details for debugging: ${JSON.stringify(httpErrorInfo)}`
       );
 
-      let errorMessage = PhotoAPI.translateStatusToErrorMessage(
+      let errorMessage = ItemAPI.translateStatusToErrorMessage(
         httpErrorInfo.status
       );
       throw new Error(errorMessage);
@@ -53,62 +94,253 @@ class PhotoAPI {
   }
 }
 
-const LOAD_PHOTOS_REQUEST = "LOAD_PHOTOS_REQUEST";
-const LOAD_PHOTOS_SUCCESS = "LOAD_PHOTOS_SUCCESS";
-const LOAD_PHOTOS_FAILURE = "LOAD_PHOTOS_FAILURE";
+// REDUX -------------------
 
+//action types
+const LOAD_ITEMS_REQUEST = 'LOAD_ITEMS_REQUEST';
+const LOAD_ITEMS_SUCCESS = 'LOAD_ITEMS_SUCCESS';
+const LOAD_ITEMS_FAILURE = 'LOAD_ITEMS_FAILURE';
+const ADD_ITEM_REQUEST = 'ADD_ITEM_REQUEST';
+const ADD_ITEM_SUCCESS = 'ADD_ITEM_SUCCESS';
+const ADD_ITEM_FAILURE = 'ADD_ITEM_FAILURE';
+const UPDATE_ITEM_REQUEST = 'UPDATE_ITEM_REQUEST';
+const UPDATE_ITEM_SUCCESS = 'UPDATE_ITEM_SUCCESS';
+const UPDATE_ITEM_FAILURE = 'UPDATE_ITEM_FAILURE';
+const DELETE_ITEM_REQUEST = 'DELETE_ITEM_REQUEST';
+const DELETE_ITEM_SUCCESS = 'DELETE_ITEM_SUCCESS';
+const DELETE_ITEM_FAILURE = 'DELETE_ITEM_FAILURE';
+
+//state (initial)
 const initialState = {
-  photos: [],
-  processing: false,
+  items: [],
+  loading: false,
   error: null,
 };
 
+//reducer
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case LOAD_PHOTOS_REQUEST:
-      return { ...state, processing: true };
-    case LOAD_PHOTOS_SUCCESS:
-      return { ...state, processing: false, photos: action.payload };
-    case LOAD_PHOTOS_FAILURE:
-      return { ...state, processing: false, error: action.payload.message };
+    case LOAD_ITEMS_REQUEST:
+      return { ...state, loading: true };
+    case LOAD_ITEMS_SUCCESS:
+      return { ...state, loading: false, items: action.payload };
+    case LOAD_ITEMS_FAILURE:
+      return { ...state, loading: false, error: action.payload.message };
+    case ADD_ITEM_REQUEST:
+      return { ...state };
+    case ADD_ITEM_SUCCESS:
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    case ADD_ITEM_FAILURE:
+      return { ...state, loading: false, error: action.payload.message };
+    case UPDATE_ITEM_REQUEST:
+      return { ...state };
+    case UPDATE_ITEM_SUCCESS:
+      return {
+        ...state,
+        items: state.items.map((item) => {
+          return item.id === action.payload.id
+            ? Object.assign({}, item, action.payload)
+            : item;
+        }),
+      };
+    case UPDATE_ITEM_FAILURE:
+      return { ...state, error: action.payload.message };
+    case DELETE_ITEM_REQUEST:
+      return { ...state };
+    case DELETE_ITEM_SUCCESS:
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload.id),
+      };
+    case DELETE_ITEM_FAILURE:
+      return { ...state, error: action.payload.message };
     default:
       return state;
   }
 }
 
-//action creator becomes thunk creator
-//instead of dispatching an action object (see commented code)
-//dispatch a thunk function (a function that returns another function)
-//inside the thunk have that function dispatch the initial request object that sets the loading
-//and eventually dispatches success and failure actions
-//by returning a function (thunk) you are now able to have the action creator do multiple dispatches over time
-function loadPhotos() {
-  // Can be any name or anonymous fn as well.
-  return (dispatch) => { 
-    let photoAPI = new PhotoAPI();
-    dispatch({ type: LOAD_PHOTOS_REQUEST });
-    return photoAPI
+//action creators
+function loadItems() {
+  return (dispatch) => {
+    let itemAPI = new ItemAPI();
+    dispatch({ type: LOAD_ITEMS_REQUEST });
+    return itemAPI
       .getAll(1)
       .then((data) => {
-        dispatch({ type: LOAD_PHOTOS_SUCCESS, payload: data });
+        dispatch({ type: LOAD_ITEMS_SUCCESS, payload: data });
       })
       .catch((error) => {
-        dispatch({ type: LOAD_PHOTOS_FAILURE, payload: error });
+        dispatch({ type: LOAD_ITEMS_FAILURE, payload: error });
       });
   };
 }
 
-const store = Redux.createStore(reducer, Redux.applyMiddleware(ReduxThunk));
-
-function logState() {
-  console.log(JSON.stringify(store.getState(), null, " "));
+function addItem(item) {
+  return (dispatch) => {
+    let itemAPI = new ItemAPI();
+    dispatch({ type: ADD_ITEM_REQUEST });
+    return itemAPI
+      .add(item)
+      .then((data) => {
+        dispatch({ type: ADD_ITEM_SUCCESS, payload: data });
+      })
+      .catch((error) => {
+        dispatch({ type: ADD_ITEM_FAILURE, payload: error });
+      });
+  };
 }
 
-store.subscribe(logState);
-
-async function test() {
-  await store.dispatch(loadPhotos());
-  console.log("loaded photos");
+function updateItem(item) {
+  return (dispatch) => {
+    let itemAPI = new ItemAPI();
+    dispatch({ type: UPDATE_ITEM_REQUEST });
+    return itemAPI
+      .update(item)
+      .then((data) => {
+        dispatch({ type: UPDATE_ITEM_SUCCESS, payload: data });
+      })
+      .catch((error) => {
+        dispatch({ type: UPDATE_ITEM_FAILURE, payload: error });
+      });
+  };
 }
 
-test();
+function deleteItem(item) {
+  return (dispatch) => {
+    let itemAPI = new ItemAPI();
+    dispatch({ type: DELETE_ITEM_REQUEST });
+    return itemAPI
+      .delete(item.id)
+      .then((data) => {
+        dispatch({ type: DELETE_ITEM_SUCCESS, payload: item });
+      })
+      .catch((error) => {
+        dispatch({ type: DELETE_ITEM_FAILURE, payload: error });
+      });
+  };
+}
+
+//store
+var ReduxThunk = window.ReduxThunk;
+const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose;
+const store = Redux.createStore(
+  reducer,
+  compose(Redux.applyMiddleware(...[ReduxThunk]))
+);
+
+// UI ---------------------------------
+function List(props) {
+  const { items, loading, error } = props;
+  const [editingItem, setEditingItem] = React.useState(null);
+  const dispatch = ReactRedux.useDispatch();
+
+  const handleEditClick = (item) => {
+    setEditingItem(item);
+  };
+
+  const handleCancel = () => {
+    setEditingItem(null);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  } else if (error) {
+    return <div>{error}</div>;
+  } else {
+    return (
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item === editingItem ? (
+              <Form item={item} onCancel={handleCancel} />
+            ) : (
+              <p>
+                <span>{item.name}</span>
+                <button onClick={() => handleEditClick(item)}>Edit</button>
+                <button onClick={() => dispatch(deleteItem(item))}>
+                  Remove
+                </button>
+              </p>
+            )}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+function Form({ item, onCancel, buttonValue }) {
+  const [inputValue, setInputValue] = React.useState(item.name || '');
+  const dispatch = ReactRedux.useDispatch();
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const isNew = !item.id;
+    const submittedItem = {
+      id: item ? item.id : ID(),
+      name: inputValue,
+    };
+
+    if (isNew) {
+      dispatch(addItem(submittedItem));
+    } else {
+      dispatch(updateItem(submittedItem));
+    }
+
+    setInputValue('');
+  };
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    onCancel();
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input value={inputValue} onChange={handleChange} />
+      <button>{buttonValue || 'Save'}</button>
+      {onCancel && (
+        <a href="#" onClick={handleCancel}>
+          cancel
+        </a>
+      )}
+    </form>
+  );
+}
+
+function Container() {
+  const items = ReactRedux.useSelector((state) => state.items);
+  const loading = ReactRedux.useSelector((state) => state.loading);
+  const error = ReactRedux.useSelector((state) => state.error);
+  const dispatch = ReactRedux.useDispatch();
+
+  React.useEffect(() => {
+    dispatch(loadItems());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Form item="" buttonValue="Add" />
+      <List loading={loading} error={error} items={items} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <ReactRedux.Provider store={store}>
+        <Container />
+      </ReactRedux.Provider>
+    </div>
+  );
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
