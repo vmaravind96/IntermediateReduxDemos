b932b03e8d793b6ce5d983752533df2f92d16382
Aravindakumar V M <aravindakumar_vijayasrimohankumar@intuit.com>
2024-09-19 14:29:01 -0700

    redux thunk example

--- main.js
+++ main.js
@@ -1,57 +1,114 @@
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
+const baseUrl = "http://localhost:3000";
 
-function increment() {
-  return { type: INCREMENT };
-}
-function decrement() {
-  return { type: DECREMENT };
+function enableDevTools() {
+  return (
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 }
 
-function reducer(state = 0, action) {
+class PhotoAPI {
+  url = `${baseUrl}/photos`;
+
+  constructor() {}
+
+  getAll(page = 1, limit = 100) {
+    return fetch(`${this.url}?_page=${page}&_limit=${limit}`)
+      .then(this.checkStatus)
+      .then(this.parseJSON);
+  }
+
+  static translateStatusToErrorMessage(status) {
+    switch (status) {
+      case 401:
+        return "Please login again.";
+      case 403:
+        return "You do not have permission to view the photos.";
+      default:
+        return "There was an error retrieving the photos. Please try again.";
+    }
+  }
+
+  checkStatus(response) {
+    if (response.ok) {
+      return response;
+    } else {
+      const httpErrorInfo = {
+        status: response.status,
+        statusText: response.statusText,
+        url: response.url,
+      };
+      console.log(
+        `logging http details for debugging: ${JSON.stringify(httpErrorInfo)}`
+      );
+
+      let errorMessage = PhotoAPI.translateStatusToErrorMessage(
+        httpErrorInfo.status
+      );
+      throw new Error(errorMessage);
+    }
+  }
+
+  parseJSON(response) {
+    return response.json();
+  }
+}
+
+const LOAD_PHOTOS_REQUEST = "LOAD_PHOTOS_REQUEST";
+const LOAD_PHOTOS_SUCCESS = "LOAD_PHOTOS_SUCCESS";
+const LOAD_PHOTOS_FAILURE = "LOAD_PHOTOS_FAILURE";
+
+const initialState = {
+  photos: [],
+  processing: false,
+  error: null,
+};
+
+function reducer(state = initialState, action) {
   switch (action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      return state - 1;
+    case LOAD_PHOTOS_REQUEST:
+      return { ...state, processing: true };
+    case LOAD_PHOTOS_SUCCESS:
+      return { ...state, processing: false, photos: action.payload };
+    case LOAD_PHOTOS_FAILURE:
+      return { ...state, processing: false, error: action.payload.message };
     default:
       return state;
   }
 }
 
-var store = Redux.createStore(reducer);
-
-
-function Counter(props) {
-  return (
-    <React.Fragment>
-      <div>Count: {props.count}</div>
-      <button onClick={props.increment}>+</button>
-      <button onClick={props.decrement}>-</button>
-    </React.Fragment>
-  );
+//action creator becomes thunk creator
+//instead of dispatching an action object (see commented code)
+//dispatch a thunk function (a function that returns another function)
+//inside the thunk have that function dispatch the initial request object that sets the loading
+//and eventually dispatches success and failure actions
+//by returning a function (thunk) you are now able to have the action creator do multiple dispatches over time
+function loadPhotos() {
+  // Can be any name or anonymous fn as well.
+  return (dispatch) => { 
+    let photoAPI = new PhotoAPI();
+    dispatch({ type: LOAD_PHOTOS_REQUEST });
+    return photoAPI
+      .getAll(1)
+      .then((data) => {
+        dispatch({ type: LOAD_PHOTOS_SUCCESS, payload: data });
+      })
+      .catch((error) => {
+        dispatch({ type: LOAD_PHOTOS_FAILURE, payload: error });
+      });
+  };
 }
 
-const mapStateToProps = (state) => {
-  return {
-    count: state,
-  };
-};
+const store = Redux.createStore(reducer, Redux.applyMiddleware(ReduxThunk));
 
-const mapDispatchToProps = { increment, decrement };
+function logState() {
+  console.log(JSON.stringify(store.getState(), null, " "));
+}
 
-const WrappedCounter = ReactRedux.connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Counter);
+store.subscribe(logState);
 
-const element = (
-  <div>
-    <ReactRedux.Provider store={store}>
-      <WrappedCounter />
-    </ReactRedux.Provider>
-  </div>
-);
+async function test() {
+  await store.dispatch(loadPhotos());
+  console.log("loaded photos");
+}
 
-const rootElement = document.getElementById('root');
-ReactDOM.createRoot(rootElement).render(element);
+test();
